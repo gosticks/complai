143739ad954a5ce77d621bc808365acdffe0d634
Wlad Meixner <hi@iamwlad.com>
2023-01-06 17:55:11 +0100

    feat: initial commit

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<div>
+	<img src="/logo.svg" width="400" />
+</div>
+
+<style global>
+	div,
+	body,
+	html {
+		width: 100%;
+		height: 100%;
+	}
+
+	div {
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
